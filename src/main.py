38a185fdd6e2bc41c1f734e27c85245efc597269
Alessandro Venturini <ales.ventus21@gmail.com>
2025-09-27 17:28:47 +0200

    reorganize code and add comments to main.py

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ from vehicle_state_machine import VehicleStateMachine
 FRAMERATE = 60
 DT = 1 / FRAMERATE
 
+
 class IO:
     _surface: pygame.Surface
     _game_display: pygame.Surface
@@ -56,20 +57,18 @@ port = os.environ.get("PORT", "2000")
 port = int(port)
 client = Client(host, port)
 client.set_timeout(120)  # pyright: ignore[reportUnknownMemberType]
+
+# Set simulator to synchronous mode and fixed time step
 world = client.get_world()
 settings = world.get_settings()
 settings.synchronous_mode = True
 settings.fixed_delta_seconds = DT
 _ = world.apply_settings(settings)
 
-spectator = world.get_spectator()
-blueprint = world.get_blueprint_library()
-vehicle_bp = blueprint.filter("vehicle.*")[0]
-spawn_point = world.get_map().get_spawn_points()[0]
-
-# Initialise the camera floating behind the vehicle
-camera_init_trans = Transform(Location(x=-5, z=3), Rotation(pitch=-20))
-camera_bp = world.get_blueprint_library().find("sensor.camera.rgb")
+blueprint_lib = world.get_blueprint_library()
+vehicle_bp = blueprint_lib.filter("vehicle.*")[0]
+vehicle_spawn_point = world.get_map().get_spawn_points()[0]
+camera_bp = blueprint_lib.find("sensor.camera.rgb")
 
 # Get camera dimensions
 image_w = camera_bp.get_attribute("image_size_x").as_int()
@@ -78,26 +77,36 @@ image_h = camera_bp.get_attribute("image_size_y").as_int()
 # Instantiate objects for rendering and vehicle control
 io = IO(image_w, image_h)
 
-# Start camera with PyGame callback
-
 try:
-    vehicle = cast(Vehicle, world.spawn_actor(vehicle_bp, spawn_point))
+    # Spawn vehicle and move spectator behind it
+    vehicle = cast(Vehicle, world.spawn_actor(vehicle_bp, vehicle_spawn_point))
+    spectator = world.get_spectator()
     spectator.set_transform(  # pyright: ignore[reportUnknownMemberType]
         Transform(
             Location(
-                x=spawn_point.location.x - 5,
-                y=spawn_point.location.y,
-                z=spawn_point.location.z + 3,
+                x=vehicle_spawn_point.location.x - 5,
+                y=vehicle_spawn_point.location.y,
+                z=vehicle_spawn_point.location.z + 3,
             ),
             Rotation(pitch=-20),
         )
     )
+
     # Spawn camera and attach it to the vehicle
     camera = cast(
-        Sensor, world.spawn_actor(camera_bp, camera_init_trans, attach_to=vehicle)
+        Sensor,
+        world.spawn_actor(
+            camera_bp,
+            Transform(Location(x=-5, z=3), Rotation(pitch=-20)),
+            attach_to=vehicle,
+        ),
     )
+
+    # Bind camera to pygame window
     camera.listen(lambda image: io.prepare_output_image(cast(Image, image)))
+
     state_machine = VehicleStateMachine(vehicle, enable_logging=True)
+
     should_exit = False
     while not should_exit:
         _ = world.tick()
@@ -116,6 +125,8 @@ try:
 finally:
     io.quit()
     remove_vehicles_and_sensors(world)
+
+    # Set simulator back to asynchronous mode and variable time step
     settings = world.get_settings()
     settings.synchronous_mode = False
     settings.fixed_delta_seconds = 0
