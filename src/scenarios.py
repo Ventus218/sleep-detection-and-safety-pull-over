e6bfadc3652b2f9ba3924c85cb0a169ef00d1e20
Eritropene <mikirava01@gmail.com>
2025-10-22 16:10:21 +0200

    add first empty scenario

--- src/scenarios.py
+++ src/scenarios.py
@@ -1,25 +1,113 @@
 
 import random
 import math
+from types import FunctionType
 import carla
-from typing import List, Tuple
-from carla import Client, World, Map, Vehicle, Transform, Location, Rotation
+from typing import List, Tuple, cast
+from carla import Client, World, Map, Vehicle, Transform, Location, Rotation, VehicleControl
 from remove_vehicles_and_sensors import remove_vehicles_and_sensors
 
 class Scenario():
-    def __init__(self, map_name: str):
-        self.map_name: str = map_name
-
-    def load(self, world: World) -> Vehicle:
-        raise NotImplementedError()
-
-# map.get_spawn_points()[349]
-
-def load_scenario(client: Client, scenario: Scenario) -> tuple[World, Vehicle]:
-    w = client.load_world_if_different(scenario.map_name)
-    world = w if w is not None else client.get_world()
-    ego_vehicle = scenario.load(world)
-
-    return (world, ego_vehicle)
+    def __init__(self, 
+                 map_name: str,
+                 spawn_point: Transform,
+                 stopped_cars: list[tuple[str, Transform]], # [(blueprint_id, spawn_point)...]
+                 moving_cars: list[tuple[str, Transform, float | None]]): # [(blueprint_id, spawn_point, speed)...]
+        self.map_name     = map_name
+        self.spawn_point  = spawn_point
+        self.stopped_cars = stopped_cars
+        self.moving_cars  =  moving_cars
 
 
+    def load(self, client: Client, ego_vehicle: Vehicle):
+        """
+        Load this scenario into the world, and setup the ego vehicle accordingly. 
+        Throws error if the map isn't the correct one.
+        """
+        # Load map
+        world = client.get_world()
+        map = world.get_map()
+        if not map.name.endswith(self.map_name):
+            raise Exception(f"Wrong map loaded! required map: {self.map_name}, actual map: {map.name}")
+        
+        spectator = world.get_spectator()
+        spectator.set_location(self.spawn_point.location)
+
+        # Create ego vehicle
+        ego_vehicle.set_transform(self.spawn_point)
+
+        blueprints = world.get_blueprint_library()
+        for v, t in self.stopped_cars:
+            vehicle = cast(Vehicle | None, world.try_spawn_actor(blueprints.find(v), t))
+            if vehicle is None:
+                continue
+            c = VehicleControl()
+            c.brake = 1
+            vehicle.apply_control(c)
+
+        traffic_manager = client.get_trafficmanager()
+        for v, t, s in self.moving_cars:
+            vehicle = cast(Vehicle | None, world.try_spawn_actor(blueprints.find(v), t))
+            if vehicle is None:
+                continue
+            vehicle.set_autopilot(True)
+            if s is not None:
+                traffic_manager.set_desired_speed(vehicle, s)
+
+    def unload(self, client: Client):
+        remove_vehicles_and_sensors(client.get_world())
+
+
+
+### SCENARIOS ###
+
+MAP04_STRAIGHT_ROAD_SPAWN_POINT = Transform(
+    Location(x=-350.343048, y=37.064049, z=0.281942),
+    Rotation(pitch=0.000000, yaw=-0.368408, roll=0.000000)
+)
+
+class EmptyStraightRoadScenario(Scenario):
+    def __init__(self):
+        p = MAP04_STRAIGHT_ROAD_SPAWN_POINT
+
+        self.obstacles = [
+            ("vehicle.chevrolet.impala", Transform(
+                Location(p.location.x + 50, p.location.y + 3, p.location.z + 1),
+                p.rotation
+            )),
+            ("vehicle.citroen.c3", Transform(
+                Location(p.location.x + 50 + 7, p.location.y + 3, p.location.z + 1),
+                p.rotation
+            )),
+            ("vehicle.audi.etron", Transform(
+                Location(p.location.x + 50 + 15, p.location.y + 3, p.location.z + 1),
+                p.rotation
+            )),
+            ("vehicle.jeep.wrangler_rubicon", Transform(
+                Location(p.location.x + 50 + 20, p.location.y + 3, p.location.z + 1),
+                p.rotation
+            )),
+            ("vehicle.volkswagen.t2_2021", Transform(
+                Location(p.location.x + 50 + 40, p.location.y + 2.8, p.location.z + 2.5),
+                p.rotation
+            )),
+            ("vehicle.chevrolet.impala", Transform(
+                Location(p.location.x + 50 + 100, p.location.y + 3, p.location.z + 5),
+                p.rotation
+            )),
+            ("vehicle.citroen.c3", Transform(
+                Location(p.location.x + 50 + 107, p.location.y + 3, p.location.z + 5),
+                p.rotation
+            )),
+            ("vehicle.audi.etron", Transform(
+                Location(p.location.x + 50 + 115, p.location.y + 3, p.location.z + 6),
+                p.rotation
+            )),
+        ]
+
+        super().__init__(
+            map_name="Town04",
+            spawn_point=MAP04_STRAIGHT_ROAD_SPAWN_POINT,
+            stopped_cars=self.obstacles,
+            moving_cars=[]
+            )
